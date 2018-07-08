c5f79b0d0aab89972ccf497fa34d4cc650085961
Serhan Uygur <serhan.uygur@gmail.com>
2018-07-08 03:57:38 +0200

    Fixed Filters to load dummy data
    And added some awesome UI bling bling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,70 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Vehicle} from './models/vehicle';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Mock Items
   vehicles: Vehicle[];
 
+  // Create Formgroup for our vehicle filters
+  filterGroup: FormGroup;
+
   // Make lists to use in app, also initialize them directly
-  brands: string[] = [];
   types: string[] = [];
+  brands: string[] = [];
   colors: string[] = [];
 
-  constructor() {
+  // holders for the selected options
+  selectedType: string;
+  selectedBrand: string;
+  selectedColor: string;
+
+  constructor(private fb: FormBuilder) {
     this.vehicles = [
       new Vehicle(0, 'Bugatti Veyron', ['red', 'green', 'orange'], '', 'car'),
       new Vehicle(1, 'Boeing 787 Dreamliner', ['yellow', 'blue', 'orange'], '', 'airplane'),
-      new Vehicle(2, 'Canadair North Star', ['black', 'yellow', 'blue', 'orange'], '', 'airplane'),
-      new Vehicle(3, 'USRA 0-6-6', ['black', 'orange', 'gray'], '', 'train'),
+      new Vehicle(2, 'Canadair North Star', ['black', 'yellow', 'blue', 'white', 'orange'], '', 'airplane'),
+      new Vehicle(3, 'USRA 0-6-6', ['black', 'orange', 'grey'], '', 'train'),
     ];
-    const vehicle = new Vehicle();
+  }
+
+  ngOnInit() {
+    this.filterGroup = this.fb.group({
+      'vehicle_type': ['none'],
+      'vehicle_brand': ['none'],
+      'vehicle_color': ['none']
+    });
+    this.selectedType = this.filterGroup.controls.vehicle_type.value;
+    this.selectedBrand = this.filterGroup.controls.vehicle_brand.value;
+    this.selectedColor = this.filterGroup.controls.vehicle_color.value;
     this.makeLists();
+    this.onChanges();
+  }
+
+  onChanges(): void {
+    this.filterGroup.valueChanges.subscribe((formResults) => {
+      this.selectedType = formResults.vehicle_type;
+      this.selectedBrand = formResults.vehicle_brand;
+      this.selectedColor = formResults.vehicle_color;
+      console.log(formResults);
+    });
   }
 
   /** Call listmaker for all the needed attributes.
    *  Clean the arrays by making every item unique. these arrays will populate the radiobuttons for the filter
    */
   makeLists() {
-    this.brands = this.listAttributes(this.vehicles, this.brands, 'brand');
     this.types = this.listAttributes(this.vehicles, this.types, 'type');
+    this.brands = this.listAttributes(this.vehicles, this.brands, 'brand');
     this.colors = this.listAttributes(this.vehicles, this.colors, 'colors');
+    console.log(this.types);
+    console.log(this.brands);
+    console.log(this.colors);
   }
 
   // Make lists form attributes of the Vehicle Array
@@ -56,6 +88,7 @@ export class AppComponent {
     } else {
       tempArr = arr;
     }
+    tempArr.unshift('none'); // first item is always none for UX purposes
 
     return tempArr.filter((item, itemIndex, me) => itemIndex === me.indexOf(item));
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [FormBuilder],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
